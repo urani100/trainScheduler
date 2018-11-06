3fd031f2aa1a51b0a716300a932e81be8f60b460
M L <mlecontenyc@gmail.com>
2018-11-06 15:27:03 -0500

    working on js from submission

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,49 @@
 // Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyBAIk3ZZC-rtF9Ad-MW3NbGPDI4GI33ojk",
+    authDomain: "counter-ad7f7.firebaseapp.com",
+    databaseURL: "https://counter-ad7f7.firebaseio.com",
+    projectId: "counter-ad7f7",
+    storageBucket: "counter-ad7f7.appspot.com",
+    messagingSenderId: "422933218698"
+  };
+  firebase.initializeApp(config);
+
 // Create a variable to reference the database
+database= firebase.database();
+
 // Initial Values
+var trainName = " ";
+var destination = " ";
+var firstTrain =0;
+var frequency = 0;
+var nextArrival = 0;
+var minutesAway = 0;
 
 // Capture Button Click
-//a prevent default
-//b// Grabb values from text boxes
-// c create object tying input to fire base objects
-//d the push object to firebase
-//e clear input fields
+$(".submitInfo").on("click", function(event){
+    //a prevent default
+    event.preventDefault();
+
+    //b// Grabb values from text boxes
+    trainName = $(".nameInput").val().trim();
+    destination =  $(".destinationInput").val().trim();
+    firstTrain = $(".FirstTrainInput").val().trim();
+    frequency = $(".frequencyInput").val().trim();
+
+    // create object tying input to fire base objects
+
+    //d the push object to firebase
+    //e clear input fields
+    $(".nameInput").val("");
+    $(".destinationInput").val("");
+    $(".FirstTrainInput").val("");
+    $(".frequencyInput").val("");
+
+})
+
+
 
 // Firebase watcher .on funtion
 // append data to table
