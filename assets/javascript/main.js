9814d099904d7ca48bd76110c037565e77301ae3
M L <mlecontenyc@gmail.com>
2018-11-06 14:32:58 -0500

    initial files

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// Create a variable to reference the database
+// Initial Values
+
+// Capture Button Click
+//a prevent default
+//b// Grabb values from text boxes
+// c create object tying input to fire base objects
+//d the push object to firebase
+//e clear input fields
+
+// Firebase watcher .on funtion
+// append data to table
